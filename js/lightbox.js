6217f012d53e47f44cc1517f88a5a1c3c2327f57
Ben McGaughey <selticesystems@gmail.com>
2015-12-31 05:50:54 -0800

    Intelligent vertical centering of lightbox images

--- js/lightbox.js
+++ js/lightbox.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
 
 		$("#lightbox-final-image").attr("src", $(this).attr("src") ) ;
 		$(".lightbox-image-container").css("width", $("#lightbox-final-image").width());
-		
+
+		var windowHeight = $( window ).height()*1;
+		var imageHeight =  $("#lightbox-final-image").height();
+
+		$("#lightbox-final-image").css("top", ((windowHeight / 2) - (imageHeight/2)) + "px");
+				
 		
 			  
 	});

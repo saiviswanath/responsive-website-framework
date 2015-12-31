728a15beeac595460552268441747fcbb91c89d9
Ben McGaughey <selticesystems@gmail.com>
2015-12-31 14:58:40 -0800

    Lightbox should be working better
    Chrome on iOS was having some issues.

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,17 +2,33 @@ $(document).ready(function(){
 
 	$( "img.lightbox-image" ).click(function() {
 				
-		$(".lightbox-overlay").show();
-		$(".lightbox-container").show();
-
-		$("#lightbox-final-image").attr("src", $(this).attr("src") ) ;
-		$(".lightbox-image-container").css("width", $("#lightbox-final-image").width());
-
-		var windowHeight = $( window ).height()*1;
-		var imageHeight =  $("#lightbox-final-image").height();
-
-		$("#lightbox-final-image").css("top", ((windowHeight / 2) - (imageHeight/2)) + "px");
 				
+		var w=0;
+		var h=0;
+		$("<img/>").attr("src", $(this).attr("src")).load(function() {
+				$(".lightbox-overlay").show();
+				$(".lightbox-container").show();
+				w = this.width*1;   
+				h = this.height*1; 
+				var neww = 0				
+				if (w >= $( window ).width())
+				{
+					neww = $( window ).width() - 20;
+					h = (h * neww) / w
+					w = neww
+				}				
+				$("#lightbox-final-image").attr("src", $(this).attr("src") ) ;	
+				$("#lightbox-final-image").css("width", w+"px");
+				$("#lightbox-final-image").css("height", h+"px");
+				$("#lightbox-final-image").css("left", ($( window ).width()/2) + "px");
+				$("#lightbox-final-image").css("margin-left", "-" + ((w/2)) + "px");
+				var windowHeight = $( window ).height()*1;
+				var imageHeight =  h;
+				var newTop
+				newTop = Math.abs((windowHeight / 2) - (imageHeight/2));
+				$("#lightbox-final-image").css("top", newTop + "px");
+
+			});		
 		
 			  
 	});
